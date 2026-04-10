eada487d22c9e77ef8f5f72ea5c9c2e638456d70
DragonRex004 <88402271+DragonRex004@users.noreply.github.com>
2026-04-10 20:59:17 +0200

    update

--- update.lua
+++ update.lua
@@ -1,11 +1,5 @@
 local base = "https://github.com/DragonRex004/scada/blob/main/"
-local files = {
-    "main.lua",
-    "startup.lua",
-    "config.lua",
-    "lib/ui.lua",
-    "lib/energy.lua"
-}
+local files = { "main.lua","startup.lua","config.lua","lib/ui.lua","lib/energy.lua" }
 
 for _, file in ipairs(files) do
     print("Updating " .. file .. "...")
